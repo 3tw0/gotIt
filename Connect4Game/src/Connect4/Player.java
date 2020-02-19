bbbf915a55db5acab9582002e59f3c954895c87e
etwo <elian.eiselt@web.de>
2020-02-19 17:52:19 +0100

    a

--- Connect4Game/src/Connect4/Player.java
+++ Connect4Game/src/Connect4/Player.java
@@ -3,59 +3,59 @@ package Connect4;
 import java.awt.Color;
 
 /**
- * Player Class.
- * Stores name, symbol, color of every player object and provides getter/setter
+ * Player Class. Stores name, symbol, color of every player object and provides
+ * getter/setter
+ * 
  * @author elianeiselt
  *
  */
 public class Player {
-	
+
 	private String name;
 	private String symbol;
 	private Color color;
 
 /////////////////
 // Constructor
-	
-	public Player () {
+
+	public Player() {
 		this("PlayerUnknown", Color.PINK);
 	}
-	
-	public Player (String name, Color color) {
+
+	public Player(String name, Color color) {
 		this.name = name;
 		this.color = color;
 	}
-	
+
 //////////////////
 // Getter / Setter
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getSymbol() {
 		return symbol;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public void setSymbol(String symbol) {		
+
+	public void setSymbol(String symbol) {
 		if (symbol.length() > 1) {
 			throw new IllegalArgumentException("Symbol musst be single char");
 		}
-		
+
 		this.symbol = " " + symbol.toUpperCase() + " ";
 	}
-	
+
 	public void setColor(Color color) {
 		this.color = color;
 	}
 	
-
 }
